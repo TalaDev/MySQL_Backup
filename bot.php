378f6f39015d980af8f47077fc9835157a04f8ab
TalaNetwork GmbH <125084137+TalaDev@users.noreply.github.com>
2023-08-28 17:09:27 +0330

    Update bot.php

--- bot.php
+++ bot.php
@@ -9,45 +9,61 @@ $dbname = "dbname";
 $bot_token = "YOUR_BOT_TOKEN";
 $chat_id = "YOUR_CHAT_ID";
 
+
 function backup_db($servername, $username, $password, $dbname, $bot_token, $chat_id) {
-  $conn = mysqli_connect($servername, $username, $password, $dbname);
-  mysqli_set_charset($conn, "utf8"); 
+  $conn = new mysqli($servername, $username, $password, $dbname);
 
-  $tables = mysqli_query($conn, 'SHOW TABLES');
-  while($row = mysqli_fetch_row($tables)) {
-    $table_array[] = $row[0];
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+
+  if (!$conn->set_charset("utf8mb4")) {
+    printf("Error loading character set utf8mb4: %s\n", $conn->error);
+    exit();
+  }
+
+  $tablesRes = $conn->query('SHOW TABLES');
+  while($row = $tablesRes->fetch_array()) {
+    $tables[] = $row[0];
   }
   
-  $sql_script = '';
-  foreach ($table_array as $table_name) {
-    
-    $structure_res = mysqli_query($conn, "SHOW CREATE TABLE $table_name");
-    $structure_row = mysqli_fetch_row($structure_res);
-    $sql_script .= "\n--\n-- Table structure for `$table_name`\n--\n";
-    $sql_script .= "DROP TABLE IF EXISTS $table_name;\n";
-    $sql_script .= $structure_row[1].";\n\n";
+  $sqlScript = '';
+  foreach ($tables as $table) {
 
-    $offset = 0;
-    $count = 0;
-    do {
-        $table_data = mysqli_query($conn, "SELECT * FROM $table_name LIMIT $offset, 100");
-        $count = mysqli_num_rows($table_data);
-        $offset += 100;
-        
-        while($row = mysqli_fetch_assoc($table_data)) {
-            $row_escaped = array();
-            foreach ($row as $key => $value) {
-                $row_escaped[$key] = "'" . mysqli_real_escape_string($conn, $value) . "'";
-            }
-            $insert_row = implode(",", $row_escaped);
-            $sql_script .= "INSERT INTO $table_name VALUES ($insert_row);\n";
+    $tableRes = $conn->query('SELECT * FROM '.$table);
+    $numFields = $tableRes->field_count;
+  
+    $sqlScript .= "DROP TABLE IF EXISTS $table;";
+    $create = $conn->query('SHOW CREATE TABLE '.$table);
+    $row2 = $create->fetch_row();
+     
+    $sqlScript .= "\n\n".$row2[1].";\n\n";
+     
+    for ($i = 0; $i < $numFields; $i++) 
+    {
+        while($row = $tableRes->fetch_row())  
+        { 
+            $sqlScript .= "INSERT INTO $table VALUES(";
+            
+            for($j=0; $j<$numFields; $j++) 
+            { 
+                $row[$j] = addslashes($row[$j]); 
+                $row[$j] = preg_replace("/\n/","\\n", $row[$j] ); 
+                
+                if (isset($row[$j])) { $sqlScript .= '"'.$row[$j].'"' ; } 
+                else { $sqlScript .= '""'; } 
+                
+                if ($j < ($numFields-1)) { $sqlScript .= ','; }
+            } 
+   
+            $sqlScript .= ");\n";
         }
-    } while ($count > 0);
-
+     }
+     $sqlScript .= "\n"; 
   }
-  
+
   $backup_file = "db_backup_" . date("Ymd") . ".sql";
-  file_put_contents($backup_file, $sql_script);
+  file_put_contents($backup_file, $sqlScript);
 
   $zip = new ZipArchive();
   $zip_file = "db_backup_" . date("Ymd") . ".zip"; 
@@ -57,6 +73,8 @@ function backup_db($servername, $username, $password, $dbname, $bot_token, $chat
 
   $url = "https://api.telegram.org/bot$bot_token/sendDocument";
   $data = array('chat_id' => $chat_id, 'document' => new CURLFile(realpath($zip_file)));
+  mysqli_set_charset($conn, 'utf8mb4');
+
 
   $ch = curl_init();
   curl_setopt($ch, CURLOPT_URL, $url);
